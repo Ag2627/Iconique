5f62010b4b64a1706e91735655dda387302b1683
Ag2627 <ananyagupta52360@gmail.com>
2024-10-29 13:47:32 +0530

    app.js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import LoginDialog from "./components/login/LoginDialog"
+
 
 function App() {
 
---
+++ src/components/login/LoginDialog.jsx
@@ -0,0 +1,11 @@
+import { Dialog } from "@mui/material"
+
+const LoginDialog = () => {
+  return (
+    <Dialog open={true}>
+        hello
+    </Dialog>
+  )
+}
+
+export default LoginDialog
