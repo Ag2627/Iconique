47cd51cf8183b5b0d9bd2bc48f1b17167df58cf7
Vani Charpe <charpe.vani@gmail.com>
2024-12-20 15:18:52 +0530

    search and filter

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,8 +1,10 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { Box, InputBase, ListItem,List, styled } from '@mui/material'
-import React, { useState,useEffect } from 'react'
+import React from 'react'
+import { useState,useEffect } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
-import { fetchProducts } from '@/redux/actions/productAction';
+import { fetchProducts as listProducts} from '@/redux/actions/productAction';
+import {Link} from 'react-router-dom';
 
 const Searchcontainer=styled(Box)`
     background:#ffffff;
@@ -20,39 +22,73 @@ const InputSearchBase=styled(InputBase)`
     padding-left:20px;
     width:100%;
     font-size:unset;
+
+`
+const ListWrapper=styled(List)`
+    position:absolute;
+    background:#FFFFFF;
+    color:#000;
+    margin-top:40px
+
 `
 const Search = () => {
   
     const [text,setText]=useState();
-    const {products}=useSelector(state=>state.fetchProducts);
+    const [open,setOpen]=useState(true);
+    
+    const fetchProducts=useSelector(state=>state.fetchProducts);
+    
+    const {products}=fetchProducts;
+    
+    //logging all products on console
+    // useEffect(() => {
+    //     if (Array.isArray(products.data)) {
+    //       for (let i = 0; i < products.data.length; i++) {
+    //         console.log(`Product ${i + 1}:`, products.data[i]);
+    //       }
+    //     }
+    //     else{
+    //         console.log("not an array");
+            
+    //     }
+    //   }, [products]); 
+    
+    
     const dispatch=useDispatch();
+
     useEffect(()=>{
-        dispatch(fetchProducts())
+        dispatch(listProducts())
     },[dispatch])
-    const [open,setOpen]=useState(false);
+   
     const getText=(text)=>{
         setText(text);
         setOpen(false);
     };
     return (
+        
     <Searchcontainer>
         <InputSearchBase 
         placeholder='Search your style,brands,TV icons and more'
-        onChange={(e)=>getText(e.target.value)}/>
+        onChange={(e)=>getText(e.target.value)}
+        value={text}/>
         <Searchiconwrapper>
             <SearchIcon/>
         </Searchiconwrapper>
         {
             text && 
-                <List>
+                <ListWrapper>
                     {
-                        products.filter(product=>product.title.toLowerCase().includes(text.toLowerCase())).map(product=>(
-                            <ListItem>
+                        products.data.filter((product)=> product.title && product.title.toLowerCase().includes(text.toLowerCase())).map(product=>(
+                            <ListItem key={product._id}>
+                                <Link to={`product/${product._id}`} onClick={()=>setText('')} style={{textDecoration:'none', color:'inherit'}}>
                                 {product.title}
+                                </Link>
                             </ListItem>
                         ))
+
+                        
                     }
-                </List>
+                </ListWrapper>
         }
     </Searchcontainer>
   )
